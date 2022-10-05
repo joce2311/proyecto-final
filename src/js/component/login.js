cce1633ef208a9a3bc63fdd6f89719393db5a006
patricia <retamalespatricia01@gmail.com>
2022-10-05 23:13:35 +0000

    contraseñas

--- src/js/component/login.js
+++ src/js/component/login.js
@@ -1,8 +1,43 @@
-
+import React, { useState, useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 const Login = () => {
-    return(
-        <div></div>
-    );
-}
-export default Login
+  const { store, actions } = useContext(Context);
+
+  return (
+    <div>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+         {/* key={index}
+        className="list-group-item d-flex justify-content-between" style=
+        {{ background: item.background }}
+        <Link to={"/single/" + index}>
+          <span>Link to: {item.title}</span>
+        </Link> 
+        <Link to="/">
+				<button className="btn btn-primary btn-lg" href="#" role="button">
+					Back home
+				</button>
+			</Link> */}
+      </Form> 
+      
+    </div>
+  );
+};
+export default Login;
--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,8 +5,17 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1">Un Manjarchs</span>
 			</Link>
+			<div>
+				
+				<Link to="/login">
+				<button className="btn btn-primary">Ingresar</button>
+				</Link>
+				<Link to="/recuperarcontraseña">
+				<button className="btn btn-primary">crear</button>
+				</Link>
+			</div>
 			<div className="ml-auto">
 				<Link to="/demo">
 					<button className="btn btn-primary">Check the Context in action</button>
