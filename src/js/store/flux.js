cdce2dc6fd870f0a3feec6881c268dad7aacd987
sneelyg <santiagoneely@gmail.com>
2022-10-07 23:03:40 +0000

    Flux contiene: login, registro

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -24,6 +24,29 @@ const getState = ({ getStore, getActions, setStore }) => {
 					fetch().then().then(data => setStore({ "foo": data.bar }))
 				*/
 			},
+			login_func: (login_email, login_password) => {
+
+				console.log("Haciendo login con" + login_email + "  clave es  " + login_password)
+				let request_body = {
+					email: login_email,
+					password: login_password
+				}
+
+				var requestOptions = {
+					method: 'POST',
+					redirect: 'follow',
+					body: request_body
+				};
+
+				console.log(requestOptions)
+
+				fetch("https://3001-sneelyg-jwtauthenticati-duxtun3mp5i.ws-us70.gitpod.io//login", requestOptions)
+					.then(response => response.json())
+					.then(result => console.log(result)) /**Se pone result.results pq la API entrega un objeto, con info y results. */
+					.catch(error => console.log('error', error));
+
+
+			},
 			changeColor: (index, color) => {
 				//get the store
 				const store = getStore();
